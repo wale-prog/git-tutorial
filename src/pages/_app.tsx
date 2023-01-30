1c40217ad899f5258b090b6c2f1c2f6ba829195f
Olawale <olapetanwale@outlook.com>
2023-01-30 12:11:27 +0100

    Create Navbar

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,11 +1,14 @@
 import { ChakraProvider } from '@chakra-ui/react';
 import type { AppProps } from 'next/app';
 import { theme } from "../chakra/theme";
+import Layout from '../Components/Layout/Layout';
 
 export default function App({ Component, pageProps }: AppProps) {
   return(
     <ChakraProvider theme={theme}>
-      <Component {...pageProps} />
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
     </ChakraProvider>
   )
 }
